deb4551d9ac8c1a22b3e1feeb3be646cae357da2
jdpenuliar <jaydz.penuliar@gmail.com>
2016-09-07 14:53:39 -0700

    server

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,"./client")));
 require("./server/config/mongoose.js");
 require("./server/config/routes.js")(app);
-
-app.listen(8000,function(){
-	console.log("haha on 8000")
+app.listen(8000, function(){
+	console.log("haha on 8000");
 });
